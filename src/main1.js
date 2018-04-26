ed675597d431bc3ac899c9a544b1158387cb6c3c
Youngwell <754052841@qq.com>
2018-04-26 21:49:44 +0800

    提交

--- src/main1.js
+++ src/main1.js
@@ -1,2 +1,18 @@
 import css from './css/index.css';
-document.getElementById('title').innerHTML = "Hello Youngwell gfdgfdgfdgfdrtetertt43535435"
+import black from './less/black.less';
+import main2 from './main2.js'
+
+/**
+ * 引入第三方库
+ * 这种引入第三方库的方式是局部引入，没有全局引入
+ * 如果要全局引入jQuery，需要使用webpack自带的ProvidePlugin插件，在webpack.config.js的plugins中进行配置
+ */
+//import $ from 'jquery'  // 这里引入是不需要我们写相对路径的，因为jquery的包是在node_modules里的，只要写一个包名jquery，系统会自动为我们查找的。需要说的是你不仅可以在入口中进行引入，你还可以在任何你需要的js中引入，webpack并不会重复打包，它只给我们打包一次。(但这说明了这种引入第三方库的方式是局部引入，没有全局引入)
+
+
+// import nav from './sass/nav.scss';
+let str = "Hello World and Webpack";
+
+document.getElementById('title').innerHTML = str;
+console.log(str);
+$('#title').html('测试jQuery引入是否成功');
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,31 @@ const path = require('path');
 const webpack = require('webpack');
 const extractPlugin = require('extract-text-webpack-plugin');
 const htmlPlugin = require('html-webpack-plugin');
+const glob = require('glob');
+const purifyCssPlugin = require('purifycss-webpack');
+const entry =  require('./webpack_config/entry_webpack.js');  // 引入自定义的一个打包入口模块
 let websit = {
     publicPath:'http://127.0.0.1:1717'
 }
+/**
+ * 切换开发模式和生产模式
+ * 在package.json中的“scripts”中设置type的值(set type=dev&webpack // 将dev赋值给type并执行webpack打包命令)
+ * process.env是node提供的一个对象
+ */
+if(process.env.type == 'dev'){  
+    console.log('开发模式')
+}else{
+    console.log('生产模式')
+}
+console.log(encodeURIComponent(process.env.type));
+
 module.exports = {
-    mode:"development",
-    entry:{
-        main:"./src/main1.js"
-    },
+    devtool: '#cheap-module-eval-source-map',  // 开启cheap-module-eval-source-map模式的sourcemap
+    mode: "development", //"production"
+    // entry:{
+    //     main:"./src/main1.js"
+    // },
+    entry: entry.path,
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'bundle.js',
@@ -18,37 +35,88 @@ module.exports = {
     module:{
         rules:[
             {
-                test: /\.css$/,
+                test: /\.(css|less)$/,
                 use: extractPlugin.extract({   // css代码分离插件
                     fallback: "style-loader",
-                    use: "css-loader",
+                    // use: "css-loader",
+                    use:[{
+                        loader: "css-loader"
+                    }, {
+                        loader: "less-loader"
+                    },
+                    // {
+                    //     loader: "sass-loader"
+                    // }
+                        'postcss-loader',  // 对css新特性，自动补全前缀，需和插件autoprefixer（自动添加前缀的插件）一起使用，此外还需在该配置文件的同级目录下新建postcss.config.js文件，默认只会补全-webkit-前缀，如需补全其他前缀需在package.json中添加browserslist参数配置
+                ],
                     publicPath:'../'    // TODO 解决分离后的css的图片url路径问题
                 })
             },
+            /*** 因为用了css代码分离插件，所以less/sass的loader配置也要写到上面的extractPlugin.extract()配置中，不然像下面这样再写一个less/sass的loader，打包时就会报错 * */
+            // {
+            //     test:/\.less$/,
+            //     use:[
+            //         {loader:'css-loader'},
+            //         {loader:'less-loader'},
+            //         {loader:'style-loader'}
+            //     ]
+            // },
+            // {
+            //     test:/\.scss$/,
+            //     use:[
+            //         {loader:'css-loader'},
+            //         {loader:'scss-loader'},
+            //         {loader:'style-loader'}
+            //     ]
+            // },
             {
                 test:/\.(jpg|png|gif)$/,
                 use:[{
-                        loader: 'url-loader',
+                        loader: 'url-loader', // url-loader中封装了file-loader
                         options:{
-                            limit:5000,
+                            limit:5000,  // 限制小于5000B的打包成base64，大于limit，url-loader会调用file-loader进行处理，参数也会直接传给file-loader。
                             name:'images/[name].[hash].[ext]'  // 设置打包后图片的路径和名字
                         }
                     }]
+            },
+            {
+                test:/\.(htm|html)$/i,
+                use:['html-withimg-loader']
+            },
+            {
+                test:/\.(jsx|js)$/,
+                use:{
+                    loader:'babel-loader',
+                    // options:{
+                    //     presets:['es2015','react']  // 建议将该配置写在.babelrc文件中
+                    // }
+                },
+                exclude:/node_modules/
             }
         ]
     },
     plugins:[
         // new webpack.HotModuleReplacementPlugin()
         new extractPlugin('css/index.css'),
-        new htmlPlugin({
-            minify: {
-                removeAttributeQuotes: true
+        new htmlPlugin({  // 打包生成html插件
+            minify:{  // 是对html文件进行压缩，removeAttrubuteQuotes是却掉属性的双引号。
+                removeAttributeQuotes:true
             },
-            hash: true,
-            template: './src/index.html'
-
+            hash:true, // 为了开发中js有缓存效果，所以加入hash，这样可以有效避免缓存JS。
+            template:'./src/index.html' //是要打包的html模版路径和文件名称。
+        }),
+        /**
+         * 清除未被使用的css插件
+         * 使用这个插件必须配合extract-text-webpack-plugin这个插件
+         */
+        new purifyCssPlugin({  
+            paths: glob.sync(path.join(__dirname, 'src/*.html'))  // 主要是寻找html模板，purifycss根据这个配置会遍历你的文件，查找哪些css被使用了。
+        }),
+        new webpack.ProvidePlugin({
+            $:'jquery'
         })
-    ],
+
+    ],  
     devServer: {
         //设置基本目录结构
         contentBase: path.resolve(__dirname, 'dist'),
